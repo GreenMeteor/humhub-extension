05a0e6556a3466b6534fe0199ab0a0571415e534
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-04-07 14:31:57 -0400

    Update index.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,13 @@
+<?php
 
+// Load the Plesk module initialization file
+require_once('/usr/local/psa/admin/plib/init.php');
+
+// Include your extension's controller file
+require_once('/usr/local/psa/admin/plib/modules/humhub-extension/controllers/IndexController.php');
+
+// Create an instance of the extension's controller and handle the request
+$controller = new \humhub\extension\controllers\IndexController();
+$controller->indexAction();
+
+?>
